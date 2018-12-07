271bd5922c59e2d37ca036c60d6ea71ffa019fbd
Karl Barrus <klbarrus@yahoo.com>
2018-12-06 19:37:14 -0500

    part 2

--- 2016-22.py
+++ 2016-22.py
@@ -18,6 +18,18 @@ def get_node(x, y, nl):
     return [nodes for nodes in nl if nodes.x == x and nodes.y == y][0]
 
 
+def search_maze(s_list, m_dict, xc, yc, steps):
+    global max_x, max_y, nodelist, zero_start
+    for xd, yd in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        x = xc + xd
+        y = yc + yd
+        if 0 <= x <= max_x and 0 <= y <= max_y:
+            nn = get_node(x, y, nodelist)
+            if nn not in m_dict and viable_node(nn, zero_start):
+                m_dict[nn] = steps + 1
+                s_list.append(nn)
+
+
 Node = namedtuple('Node', 'x y size used avail percent')
 nodelist = []
 with open(sys.argv[1]) as f:
@@ -32,7 +44,6 @@ print('part 1: {} viable nodes'.format(numv))
 
 max_x = max(node.x for node in nodelist)
 max_y = max(node.y for node in nodelist)
-print('grid dimensions: {} x {}'.format(max_x, max_y))
 data_start = get_node(max_x, 0, nodelist)
 print('data starts at {}'.format(data_start))
 zero_start = [nodes for nodes in nodelist if nodes.used == 0][0]
@@ -54,4 +65,25 @@ for x in range(0, max_x + 1):
             row += '.'
     print('{}'.format(row))
 
-            
+# find shortest path for empty node to move adjacent (one space above) data
+min_steps = 0
+adjacent_data = get_node(data_start.x - 1, 0, nodelist)
+steps_count = {zero_start: 0}
+search = [zero_start]
+while search:
+    curr = search[0]
+    curr_steps = steps_count[curr]
+    search = search[1:]
+    search_maze(search, steps_count, curr.x, curr.y, curr_steps)
+    if adjacent_data in steps_count:
+        min_steps = steps_count[adjacent_data]
+        break
+
+# steps to move data to adjacent to goal (one space below)
+min_steps += 5 * (data_start.x - 1)
+
+# steps for final swap
+min_steps += 1
+
+print('part 2: {} steps'.format(min_steps))
+
